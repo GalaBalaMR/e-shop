86c4b6d92604f325f14d6063d008f25208d7e81c
GalaBalaMR <matus.recka@gmail.com>
2022-11-21 09:29:34 +0100

    add event for updated(if 10pcs, send message admin)

--- app/Models/Item.php
+++ app/Models/Item.php
@@ -12,7 +12,7 @@ class Item extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['name', 'short_description', 'long_description', 'numbers', 'price','img'];
+    protected $fillable = ['name', 'short_description', 'long_description', 'storage_pcs', 'price','img'];
 
     public function category()
     {
@@ -27,5 +27,26 @@ class Item extends Model
     public function order()
     {
         return $this->belongsToMany(Order::class);
+    }
+
+    // for decimals in price
+    public function getPriceAttribute($price)
+    {
+        return number_format($price, 2);
+    }
+
+    public static function boot() {
+        parent::boot();
+
+        // Message for admin, about storage_pcs is under 10 pcs
+        static::updated(function($item) {
+            if($item->storage_pcs < 10)
+            Message::create([
+                'name' => 'Nedostatok produktu '. $item->name,
+                'about' => 'item',
+                'about_id' => $item->id,
+                'content' => 'Zásoby produktu '. $item->name .' klesli pod 10 kusov. Aktuálne zásoby sú '.          $item->storage_pcs .'.'
+            ]);
+        });        
     }
 }
