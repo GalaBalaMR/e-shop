e112d567659e5020dfe5fc8cb64c0dfcfc56fa94
GalaBalaMR <matus.recka@gmail.com>
2022-11-21 09:32:45 +0100

    add updated event for message admin and send mail to customer

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -3,8 +3,13 @@
 namespace App\Models;
 
 use App\Models\Item;
+use App\Models\Message;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Support\Facades\Mail;
+use App\Mail\OrderConfirm;
+
+
 
 class Order extends Model
 {
@@ -27,4 +32,20 @@ class Order extends Model
         return $this->hasOne(Address::class);
     }
 
+    public static function boot() {
+        parent::boot();
+
+        // message for admin, about new order
+        static::created(function($order) {
+            Message::create([
+                'name' => 'Nová objednávka-',
+                'about' => 'order',
+                'about_id' => $order->id,
+                'content' => 'Zákazník '. auth()->user()->name .' si objednal tovar.'
+            ]);
+
+            // Send mail to customer about success order
+            Mail::to( $order->user->email)->send( new OrderConfirm($order));
+        });        
+    }
 }
