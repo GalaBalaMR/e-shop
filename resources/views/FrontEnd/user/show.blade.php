628006bbc6150577c55286e0e279d42433d23352
Matus <matus.recka@gmail.com>
2022-12-01 17:05:56 +0100

    small change in view

--- resources/views/FrontEnd/user/show.blade.php
+++ resources/views/FrontEnd/user/show.blade.php
@@ -5,17 +5,15 @@
 <section id="portfolio-details" class="portfolio-details">
     <div class="container">
 
-    <div class="row gy-4">
+    <div class="row gy-4 d-flex justify-content-center">
 
         <div class="col-lg-4">
         <div class="portfolio-info">
             <h3>Informácie o používateľovi</h3>
             <ul>
                 <li><strong>Meno: </strong>{{ $user->name }}</li>
-                <li><strong>Email: </strong>: {{ $user->email }}</li>
-                <li><strong>Číslo:</strong>: {{ $user->phone }}</li>
-                <li>
-                </li>
+                <li><strong>Email: </strong> {{ $user->email }}</li>
+                <li><strong>Číslo:</strong> {{ $user->phone === 0 ? $user->phone : 'Nie je zadané číslo' }}</li>
             </ul>
         </div>
         </div>
