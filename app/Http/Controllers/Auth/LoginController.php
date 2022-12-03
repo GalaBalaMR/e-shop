584eb615e7d9184896fef1203f1b67f7eb683692
Matus <matus.recka@gmail.com>
2022-12-03 18:55:01 +0100

    add function for github registration

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers\Auth;
 
+use App\Models\User;
+use Illuminate\Support\Str;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Hash;
 use App\Providers\RouteServiceProvider;
+use Laravel\Socialite\Facades\Socialite;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
 class LoginController extends Controller
@@ -36,5 +41,31 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    public function github()
+    {
+        // send user's request to github
+        return Socialite::driver('github')->redirect();
+    }
+
+    public function githubRedirect()
+    {
+        // send user's request to github
+        $user = Socialite::driver('github')->user();
+
+        // if the user doesn't exist, add them
+        // if they do, get the model
+        // either way, authenticate the user into the aplication and redirect
+        $user = User::firstOrCreate([
+            'email' => $user->email
+        ], [
+            'name' => $user->nickname,
+            'password' => Hash::make(Str::random(24))
+        ]);
+
+        Auth::login($user, true);
+
+        return redirect('/');
     }
 }
